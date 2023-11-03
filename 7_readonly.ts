15d403890a676176d1044377dc5a80881d1cb1dd
rcncln <53015856+rcncln@users.noreply.github.com>
2023-11-03 18:08:21 +0100

    7 Readonly solution

---
+++ 7_readonly.ts
@@ -0,0 +1,19 @@
+type MyReadonly<T> = {
+    readonly [Key in keyof T]:T[Key]
+}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
